b75322f4c11ceb7443529ebe25a8dff36be167fb
B-Sallum <brusallum@hotmail.com>
2021-10-31 12:07:14 -0300

    simple css working

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const frontList = document.getElementById('list');
+const frontList = document.getElementById('tapeList');
 const apiUrl = 'http://localhost:3000/';
 
 const getGamesList = async () => {
@@ -9,12 +9,25 @@ const getGamesList = async () => {
     frontList.insertAdjacentHTML('beforeend', `
     <div class="card">
       <img src="${game.imgUrl}" alt="">
-      <h2 class="title">${game.name}</h2>
-      <div class="year">${game.year}</div>
-      <div class="category">${game.category}</div>
+      <div class="card-info">
+        <h3 class="title">${game.name}</h3>
+        <div class="year-rating">
+          <div class="year">${game.year}</div>
+          <div class="rating">${game.rating}</div>
+        </div>
+        <div class="category">${game.category}</div>
+      </div>
     </div>
     `);
   });
 };
 
 getGamesList();
+
+// ${game.id}
+// ${game.name}
+// ${game.category}
+// ${game.year}
+// ${game.imgUrl}
+// ${game.havePlay}
+// ${game.rating}
