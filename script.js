15867221346782e2884a5ceb86d563038c239972
B-Sallum <brusallum@hotmail.com>
2021-11-03 18:38:11 -0300

    sistema estagnado, apenas faz GET

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
+
+const API_URL = 'http://localhost:3000/';
+
 const frontList = document.getElementById('tapeList');
-const apiUrl = 'http://localhost:3000/';
 
 const getGamesList = async () => {
-  const response = await fetch(apiUrl);
+
+  const response = await fetch(API_URL);
   const gamesList = await response.json();
-  console.log(gamesList);
+
   gamesList.map((game) => {
     frontList.insertAdjacentHTML('beforeend', `
     <div class="card-out" style="background-image: url('${game.imgUrl}'); background-size: cover; background-position: center;">
       <div class="card-in">
+        <input type="hidden" id="${game.id}">
         <div class="category"><h3>${game.category}</h3></h4>
         <div class="year"><h3>${game.year}</h3></div>
-        <div class="rating"><h3>${game.rating}</h3></div>
         <div class="name"><h3>${game.name}</h3></div>
+        <div class="row">
+          <input type="checkbox" name="havePlay" id="havePlay" ${game.havePlay} />
+          <input type="number" class="formBox" name="rating" id="rating" value="${game.rating}" />          
+        </div>
       </div>
     </div>
     `);
   });
 };
 
-const gamePlay = () => {
-  
-}
-
 getGamesList();
 
-// ${game.id}
-// ${game.name}
-// ${game.category}
-// ${game.year}
-// ${game.imgUrl}
-// ${game.havePlay}
-// ${game.rating}
